4d59f7d0278943d0ea13b34945ea785163624872
Sudip Bohara <sudipbohara502@gmail.com>
2026-04-24 20:24:16 +0545

    Revert "refactor: restructure project into WATOS directory and update role system"

--- WATOS/watos-backend/app/ml/pert.py
+++
@@ -1,9 +0,0 @@
-def pert_estimate(optimistic: float, most_likely: float, pessimistic: float) -> dict:
-    expected = (optimistic + 4 * most_likely + pessimistic) / 6
-    std_dev = (pessimistic - optimistic) / 6
-    return {
-        "expected_hours": round(expected, 2),
-        "std_dev": round(std_dev, 2),
-        "low_95": round(expected - 2 * std_dev, 2),
-        "high_95": round(expected + 2 * std_dev, 2),
-    }
